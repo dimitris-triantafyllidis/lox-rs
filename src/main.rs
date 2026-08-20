5cafa0adeaf8ab7298c90f217225f83c490ac068
Dimitris Triantafyllidis <dimitris.i.triantafyllidis@outlook.com>
2026-08-20 11:05:11 +0300

    A first skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,61 @@
-use std::process::ExitCode;
+use std::{env, fs, io, process::ExitCode};
+
+use rustyline::error::ReadlineError;
+use rustyline::DefaultEditor;
 
 fn main() -> ExitCode {
+
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 2 {
+        println!("Usage: lox [script]");
+        return ExitCode::from(64);
+    }
+    else if args.len() == 2 {
+        run_file(&args[1]);
+    }
+    else {
+        run_repl();
+    }
+
     return ExitCode::from(0);
+}
+
+fn run_file(file_path: &String) {
+
+    match fs::read_to_string(file_path) {
+        io::Result::Ok(s) => {
+            run(&s);
+        },
+        io::Result::Err(e) => {
+            eprintln!("io error: {}", e);
+        }
+    }
+}
+
+fn run_repl() {
+
+    let mut rl = DefaultEditor::new().unwrap();
+
+    loop {
+        match rl.readline("lox > ") {
+
+            Ok(line) => {
+                run(&line);
+            }
+            Err(ReadlineError::Interrupted) => {
+                continue;
+            }
+            Err(ReadlineError::Eof) => {
+                break;
+            }
+            Err(e) => {
+                eprintln!("rustyline error: {}", e);
+            }
+        }
+    }
+}
+
+fn run(_s: &String) {
+
 }
